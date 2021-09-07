0430ea1b0e5ed675af7549fa9fe8a153f2db8061
Rafael Batista <rafaabatistas@gmail.com>
2021-09-07 14:12:16 -0300

    FEAT: Atualizando o arquivo _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,23 @@ import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 import GlobalStyles from '~styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from '~styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>Boilerplate NextJS</title>
-        <link rel="shortcut icon" href="assets/img/icon-react.png" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="assets/img/icon-react.png" type="image/x-icon" />
+        <title>98Barbershop</title>
+        <link rel="shortcut icon" href="assets/img/icon-logo.png" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="assets/img/icon-logo.png" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#19062b" />
-        <meta name="description" content="Simples projeto iniciado com Typescript, NextJS, React, Styled-Components" />
+        <meta name="theme-color" content="#333333" />
+        <meta name="description" content="98Barbershop, sua barbearia preferida agora em uma plataforma." />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
