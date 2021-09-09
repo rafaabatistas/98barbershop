1b9b39d0753f89e3bd9c69773f907f1ace5279d9
Gabriel Guedes <gabrielrguedess@gmail.com>
2021-09-08 21:49:46 -0300

    REFACTOR: Refatorarando o componente <NavList />

--- src/components/ui/molecules/NavList/NavList.styles.ts
+++ src/components/ui/molecules/NavList/NavList.styles.ts
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
 
 export const NavList = styled.ul`
+  position: relative;
   width: 100%;
   height: 100%;
   list-style: none;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  overflow-x: hidden;
+
+  ${media.greaterThan('small')`
+    justify-content: center;
+  `}
 `;
--- src/components/ui/molecules/NavList/NavList.tsx
+++ src/components/ui/molecules/NavList/NavList.tsx
@@ -1,19 +1,41 @@
-import * as S from './NavList.styles';
+import { useState } from 'react';
 
 import Fade from 'react-reveal/Fade';
+import { Sling as Hamburger } from 'hamburger-react';
 
+import MediaMatch from '../MediaMatch/MediaMatch';
 import { Logotipo } from '~atoms/Logotipo/Logotipo';
-import { NavItem } from '~atoms/NavItem/NavItem';
+
+import * as S from './NavList.styles';
+import { MenuMobile } from '../MenuMobile/MenuMobile';
 
 export const NavList = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <S.NavList>
       <Fade left>
         <Logotipo hideOnMobile />
       </Fade>
-      <Fade right>
-        <NavItem>O melhor estar por vir!</NavItem>
-      </Fade>
+
+      <MediaMatch lessThan="medium">
+        <Fade right>
+          <Hamburger
+            color="#FFF"
+            duration={0.4}
+            size={30}
+            distance="md"
+            rounded
+            direction="right"
+            easing="ease-out"
+            toggled={isOpen}
+            toggle={setIsOpen}
+            label="Abrir e Fechar"
+          />
+
+          <MenuMobile isOpen={isOpen} />
+        </Fade>
+      </MediaMatch>
     </S.NavList>
   );
 };
