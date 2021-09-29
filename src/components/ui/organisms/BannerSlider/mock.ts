a3d0ad33ecdc2261b73c84dd184bed31429b414e
Rafael <rafaabatistas@gmail.com>
2021-09-29 09:48:07 -0300

    FIX: Atualizando as informações do mock do Banner

--- src/components/ui/organisms/BannerSlider/mock.ts
+++ src/components/ui/organisms/BannerSlider/mock.ts
@@ -4,46 +4,37 @@ const items: BannerProps[] = [
   {
     img: '/assets/img/slide-1.jpg',
     title: 'Bem Vindo a BarberShop',
-    subtitle: 'We Are BarberShop!',
-    description: 'Enjoy the Ultimate Relaxed Grooming Experience For Men every time you step into the Boardroom',
+    subtitle: 'Agende já o seu horário',
+    description: '',
     buttons: 2,
-    buttonLabelOne: 'Marcar Hora',
+    buttonLabelOne: 'Marcar Horário',
     buttonLabelTwo: 'Saiba Mais'
   },
   {
     img: '/assets/img/slide-2.jpg',
     title: 'Bem Vindo a BarberShop',
-    subtitle: 'We Are BarberShop!',
-    description: 'Enjoy the Ultimate Relaxed Grooming Experience For Men every time you step into the Boardroom',
+    subtitle: 'Comece a semana se cuidando',
+    description: '',
     buttons: 2,
-    buttonLabelOne: 'Marcar Hora',
+    buttonLabelOne: 'Marcar Horário',
     buttonLabelTwo: 'Saiba Mais'
   },
   {
     img: '/assets/img/slide-3.jpg',
     title: 'Bem Vindo a BarberShop',
-    subtitle: 'We Are BarberShop!',
-    description: 'Enjoy the Ultimate Relaxed Grooming Experience For Men every time you step into the Boardroom',
+    subtitle: 'Barba bem feita, todo mundo respeita',
+    description: '',
     buttons: 2,
-    buttonLabelOne: 'Marcar Hora',
+    buttonLabelOne: 'Marcar Horário',
     buttonLabelTwo: 'Saiba Mais'
   },
   {
     img: '/assets/img/slide-4.jpg',
     title: 'Bem Vindo a BarberShop',
-    subtitle: 'We Are BarberShop!',
-    description: 'Enjoy the Ultimate Relaxed Grooming Experience For Men every time you step into the Boardroom',
+    subtitle: 'Equilíbrio entre a modernidade e tradição',
+    description: '',
     buttons: 2,
-    buttonLabelOne: 'Marcar Hora',
-    buttonLabelTwo: 'Saiba Mais'
-  },
-  {
-    img: '/assets/img/slide-5.jpg',
-    title: 'Bem Vindo a BarberShop',
-    subtitle: 'We Are BarberShop!',
-    description: 'Enjoy the Ultimate Relaxed Grooming Experience For Men every time you step into the Boardroom',
-    buttons: 2,
-    buttonLabelOne: 'Marcar Hora',
+    buttonLabelOne: 'Marcar Horário',
     buttonLabelTwo: 'Saiba Mais'
   }
 ];
