2d54ef527181ad81687badffa8873e94d653ba24
Rafael <rafaabatistas@gmail.com>
2021-10-02 23:31:29 -0300

    FEAT: Adaptando link do react-scroll nos links no componente <NavLinks />

--- src/components/ui/molecules/NavLinks/NavLinks.test.tsx
+++ src/components/ui/molecules/NavLinks/NavLinks.test.tsx
@@ -6,7 +6,17 @@ import { NavLinks } from './NavLinks';
 describe('<NavLinks />', () => {
   it('Deve renderizar todos os componentes dentro do componente <NavLinks />', () => {
     const { container } = renderWithTheme(
-      <NavLinks paddingXLine={10} names={['Home', 'Sobre', 'Serviços', 'Equipe', 'Opinião', 'FeedBack']}>
+      <NavLinks
+        paddingXLine={10}
+        names={[
+          { nav: 'Home', link: 'home' },
+          { nav: 'Sobre', link: 'sobre' },
+          { nav: 'Serviços', link: 'servicos' },
+          { nav: 'Equipe', link: 'equipe' },
+          { nav: 'Opinião', link: 'opiniao' },
+          { nav: 'FeedBack', link: 'feedBack' }
+        ]}
+      >
         <h1>Logo</h1>
       </NavLinks>
     );
@@ -28,69 +38,67 @@ describe('<NavLinks />', () => {
 
   it('Verificar hover do menu do componente <NavLinks />', () => {
     renderWithTheme(
-      <NavLinks paddingXLine={10} names={['Home', 'Sobre', 'Serviços', 'Equipe', 'Opinião', 'FeedBack']}>
+      <NavLinks
+        paddingXLine={10}
+        names={[
+          { nav: 'Home', link: 'home' },
+          { nav: 'Sobre', link: 'sobre' },
+          { nav: 'Serviços', link: 'servicos' },
+          { nav: 'Equipe', link: 'equipe' },
+          { nav: 'Opinião', link: 'opiniao' },
+          { nav: 'FeedBack', link: 'feedBack' }
+        ]}
+      >
         <h1>Logo</h1>
       </NavLinks>
     );
 
     //Por Padrao ativado HOME
-    expect(screen.getByText(/Home/i)).toHaveStyle({
-      color: 'rgb(247, 24, 18)'
-    });
+    expect(screen.getByText('Home')).toHaveStyleRule('color', '#1affea', { modifier: ':hover' });
 
     //Clicar no Sobre
     fireEvent.click(screen.getByText(/Sobre/i));
 
-    expect(screen.getByText(/Home/i)).toHaveStyle({
-      color: 'rgb(255, 255, 255)'
+    expect(screen.getByText('Home')).toHaveStyle({
+      color: ''
     });
 
-    expect(screen.getByText(/Sobre/i)).toHaveStyle({
-      color: 'rgb(247, 24, 18)'
-    });
+    expect(screen.getByText(/Sobre/i)).toHaveStyleRule('color', '#1affea', { modifier: ':hover' });
 
     //Clicar no Serviços
     fireEvent.click(screen.getByText(/Serviços/i));
 
     expect(screen.getByText(/Sobre/i)).toHaveStyle({
-      color: 'rgb(255, 255, 255)'
+      color: ''
     });
 
-    expect(screen.getByText(/Serviços/i)).toHaveStyle({
-      color: 'rgb(247, 24, 18)'
-    });
+    expect(screen.getByText(/Serviços/i)).toHaveStyleRule('color', '#1affea', { modifier: ':hover' });
 
     //Clicar no Equipe
     fireEvent.click(screen.getByText(/Equipe/i));
 
     expect(screen.getByText(/Serviços/i)).toHaveStyle({
-      color: 'rgb(255, 255, 255)'
+      color: ''
     });
 
-    expect(screen.getByText(/Equipe/i)).toHaveStyle({
-      color: 'rgb(247, 24, 18)'
-    });
+    expect(screen.getByText(/Equipe/i)).toHaveStyleRule('color', '#1affea', { modifier: ':hover' });
 
     //Clicar no Opinião
     fireEvent.click(screen.getByText(/Opinião/i));
 
     expect(screen.getByText(/Equipe/i)).toHaveStyle({
-      color: 'rgb(255, 255, 255)'
+      color: ''
     });
 
-    expect(screen.getByText(/Opinião/i)).toHaveStyle({
-      color: 'rgb(247, 24, 18)'
-    });
+    expect(screen.getByText(/Opinião/i)).toHaveStyleRule('color', '#1affea', { modifier: ':hover' });
 
     //Clicar no FeedBack
     fireEvent.click(screen.getByText(/FeedBack/i));
 
     expect(screen.getByText(/Opinião/i)).toHaveStyle({
-      color: 'rgb(255, 255, 255)'
+      color: ''
     });
 
-    expect(screen.getByText(/FeedBack/i)).toHaveStyle({
-      color: 'rgb(247, 24, 18)'
-    });
+    expect(screen.getByText(/FeedBack/i)).toHaveStyleRule('color', '#1affea', { modifier: ':hover' });
   });
 });
