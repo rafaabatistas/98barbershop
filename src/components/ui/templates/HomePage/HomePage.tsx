57b42733261eac40c934eb588d4a534f6f144a57
Rafael <rafaabatistas@gmail.com>
2021-10-11 17:43:27 -0300

    FIX: Adicionando <SectionAbout /> na HomePage

--- src/components/ui/templates/HomePage/HomePage.tsx
+++ src/components/ui/templates/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import { Element } from 'react-scroll';
 import { Header } from '~organisms/Header/Header';
 import { Footer } from '~organisms/Footer/Footer';
 import { BannerSlider } from '~organisms/BannerSlider/BannerSlider';
+import { SectionAbout } from '~organisms/SectionAbout/SectionAbout';
 import { MainServices } from '~organisms/MainServices/MainServices';
 import items from '~organisms/BannerSlider/mock';
 import services from '~organisms/MainServices/mock';
@@ -13,6 +14,9 @@ const HomePage = () => {
       <Header />
       <Element name="home">
         <BannerSlider items={items} />
+      </Element>
+      <Element name="sobre">
+        <SectionAbout />
       </Element>
       <Element name="servicos">
         <MainServices items={services} />
