d413d64e6ea9dd6d1879669c6dfe0cdb7c64157c
Gabriel Guedes <gabrielrguedess@gmail.com>
2021-10-12 19:11:07 -0300

    FEAT: Adicionando novos atributos ao componente <Footer />

--- src/components/ui/organisms/Footer/Footer.tsx
+++ src/components/ui/organisms/Footer/Footer.tsx
@@ -1,13 +1,23 @@
 import * as S from './Footer.styles';
 
 import Fade from 'react-reveal/Fade';
+import Link from 'next/link';
 
 import { SocialMedia } from '~atoms/SocialMedia/SocialMedia';
-import { Description } from '~atoms/Description/Description';
+import { Container } from '~atoms/Container/Container';
+import { Logotipo } from '../../atoms/Logotipo/Logotipo';
 
 export const Footer = () => (
   <S.Footer>
-    <S.Container>
+    <Container>
+      <S.BoxLogo>
+        <Logotipo />
+        <S.Description size="normal">
+          A 98 BarberShop é um espaço de serviços masculinos que combina valores tradicionais com estilo moderno. Uma
+          barbearia contemporânea com uma atmosfera informal e alegre.
+        </S.Description>
+      </S.BoxLogo>
+
       <S.BoxInfoSocialMedia>
         <Fade>
           <S.Heading>Siga-nos</S.Heading>
@@ -26,22 +36,30 @@ export const Footer = () => (
           </S.BoxSocialMedia>
         </Fade>
       </S.BoxInfoSocialMedia>
-      <S.BoxInfoLocation>
-        <Fade>
-          <Description size="medium" marginBottom>
-            Rua Caminho Encanto 334 - Guarulhos - SP
-          </Description>
-          <Description size="medium" marginBottom>
-            Copyright ©2021 <span>98Barbershop</span>. Todos os direitos reservados
-          </Description>
-        </Fade>
-      </S.BoxInfoLocation>
       <S.BoxInfoInstitutional>
-        <Fade>
-          <S.Heading>Institucional</S.Heading>
-          <Description size="xmedium">Termos de Uso</Description>
-        </Fade>
+        <S.Heading>Institucional</S.Heading>
+        <Link href="#">
+          <a>Termos de Uso</a>
+        </Link>
+        <Link href="#">
+          <a>Política de Privacidade</a>
+        </Link>
       </S.BoxInfoInstitutional>
-    </S.Container>
+    </Container>
+    <S.BoxInfoLocation>
+      <S.Curve xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 24">
+        <path d="M50.49 0C40.096 0 8.415 23.94 0 23.94h100.486C92.566 23.94 60.886 0 50.49 0Z" fill="currentColor" />
+      </S.Curve>
+      <S.BoxIcon to="home">
+        <S.Arrow size={25} />
+      </S.BoxIcon>
+
+      <Fade>
+        <S.Description size="small">Rua Caminho Encanto 334 - Guarulhos - SP</S.Description>
+        <S.Description size="small">
+          Copyright ©2021 <span>98Barbershop</span>. Todos os direitos reservados
+        </S.Description>
+      </Fade>
+    </S.BoxInfoLocation>
   </S.Footer>
 );
