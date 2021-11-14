3bb53ce76f2f28b474c269efebb165080b469f13
Rafael Batista <rafaabatistas@gmail.com>
2021-11-14 00:23:39 -0300

    REFACTOR: Atualizando as props e o retorno da function getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 
 import client from '~graphql/client';
-import { GET_VIEWS } from '~graphql/queries';
+import { GET_REVIEWS } from '~graphql/queries';
 import { GetReviewsProps } from '~graphql/types/graphql';
 import { ReviewContext } from '~contexts/ReviewContext';
 import { ReviewProps } from '~organisms/Review/Review';
@@ -17,15 +17,15 @@ export default function Home({ reviews }: ReviewProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { allContacts } = await client.request<GetReviewsProps>(GET_VIEWS, { first: 6 });
+  const { allEvaluations } = await client.request<GetReviewsProps>(GET_REVIEWS, { first: 6 });
 
-  const reviews = allContacts.map((review) => {
+  const reviews = allEvaluations.map((review) => {
     return {
-      atendidoPor: review.atendidoPor,
-      nota: review.nota,
+      answered: review.answered,
+      stars: review.stars,
       feedback: review.feedback,
-      nome: review.nome,
-      sobrenome: review.sobrenome
+      name: review.name,
+      surname: review.surname
     };
   });
 
