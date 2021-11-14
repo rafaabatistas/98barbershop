f556af12ecf94e2d5236fc50f85177537ba5c495
Rafael Batista <rafaabatistas@gmail.com>
2021-11-14 00:19:40 -0300

    FIX: Removendo arquivo do client do datocms

--- src/dato-cms/dato-client.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { SiteClient } = require('datocms-client');
-
-const datoClient = new SiteClient(process.env.NEXT_PUBLIC_GRAPHQL_TOKEN);
-
-export default datoClient;
